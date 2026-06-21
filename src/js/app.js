d6bf4ddf5064fd6fd00203e5b38e901046376027
Toni Fey <ToniFey@protonmail.com>
2026-06-21 15:27:43 +0200

    feat: integrate news fetching functionality and update landing page structure

--- src/js/app.js
+++ src/js/app.js
@@ -6,6 +6,10 @@
 */
 
 const finaleTimestamp = 1784487600;
+const newsAPI = "https://site.api.espn.com/apis/site/v2/sports/soccer/fifa.world/news";
+const teamsAPI = "https://worldcup26.ir/get/teams";
+const gamesAPI = "https://worldcup26.ir/get/games";
+const newsBaseURL = "https://www.espn.com/soccer/story/_/id/";
 
 /// A function to automatically calculate the time difference between now and the finale
 /// of the 2026 World Cup and displaying it. Also hides the whole card when we are past this
@@ -55,7 +59,7 @@ let futureGames = [];
 let teamsData = [];
 
 async function getGameData() {
-    let gameData = await fetchFromURL("https://worldcup26.ir/get/games");
+    let gameData = await fetchFromURL(gamesAPI);
     if (gameData == null) {
         console.error("Could not fetch games data")
         return;
@@ -87,8 +91,55 @@ async function getGameData() {
     console.log(finishedGames);
     console.log(futureGames);
 
-    teamsData = await fetchFromURL("https://worldcup26.ir/get/teams");
+    teamsData = await fetchFromURL(teamsAPI);
     console.log(teamsData);
 }
 
+async function getNews() {
+    let newsData = await fetchFromURL(newsAPI);
+    if (newsData == null) {
+        console.error("Could not fetch news data")
+        return;
+    }
+    console.log(newsData);
+
+    let newsContainer = document.getElementById("newsContainer");
+
+    for (let i = 0; i < 2; i++) {
+        let article = newsData['articles'][i];
+
+        let newsCard = document.createElement('article');
+        newsCard.classList.add("card");
+        newsCard.classList.add("card--news");
+        newsCard.classList.add("card--interactive");
+
+        let newsImage = document.createElement('img');
+        newsImage.alt = article['images'][0]['alt'];
+        newsImage.src = article['images'][0]['url'];
+
+        let newsHeader = document.createElement('h4');
+        newsHeader.textContent = article['headline'];
+
+        let newsDescription = document.createElement('p');
+        newsDescription.textContent = article['description'];
+
+        let newsAnchor = document.createElement('a');
+        newsAnchor.href = article['links']['web']['href'];
+        newsAnchor.textContent = "Read more ";
+
+        let newsAnchorChevron = document.createElement('i');
+        newsAnchorChevron.classList.add("fa");
+        newsAnchorChevron.classList.add("fa-chevron-right");
+
+        newsAnchor.append(newsAnchorChevron);
+        newsCard.append(newsImage, newsHeader, newsDescription, newsAnchor);
+        newsContainer.append(newsCard);
+
+        newsCard.addEventListener("click", function () {
+            window.location.href = article['links']['web']['href'];
+        });
+    }
+}
+
 getGameData();
+getNews();
