3dd750c97777e9dafeefab267979889adc4a8ffa
Toni Fey <ToniFey@protonmail.com>
2026-06-24 18:37:18 +0200

    feat: enhance schedule match card styles and layout for improved user experience

--- config/stylelint.config.mjs
+++ config/stylelint.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import("stylelint").Config} */
 export default {
-  extends: ["stylelint-config-standard"]
+  extends: ["stylelint-config-standard"],
+  rules: {
+    "color-hex-length": null,
+    "comment-empty-line-before": null,
+    "rule-empty-line-before": null,
+    "declaration-empty-line-before": null,
+    "custom-property-empty-line-before": null,
+    "declaration-block-single-line-max-declarations": null
+  }
 };
